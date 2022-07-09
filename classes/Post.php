9df0c529b51624a3f83e9866f568869a2f5ea4db
CE5RIC <cedricdierckx5@hotmail.com>
2022-07-09 13:32:16 +0200

    Getters added

--- classes/Post.php
+++ classes/Post.php
@@ -1,20 +1,35 @@
 <?php
 declare(strict_types=1);
 
-
-class Post {
-
+class Post
+{
     private string $title;
     private string $content;
     private string $authorName;
     private string $date;
 
 
-public function __construct(string $title, string $content, string $authorName, string $date) {
-    $this->title = $title;
-    $this->content = $content;
-    $this->authorName = $authorName;
-    $this->date = $date;
-}
+    public function __construct(string $title, string $content, string $authorName ,string $date)
+    {
+        $this->title = $title;
+        $this->content = $content;
+        $this->authorName = $authorName;
+        $this->date = $date;
+    }
+
+    public function getTitle (): string
+    {
+        return $this->title;
+    }
+    public function getContent () : string {
+        return $this->content;
+    }
+    public function getAuthorName () : string {
+        return $this->authorName;
+    }
+    public function getDate () : string {
+        return $this->date;
+    }
 
 }
+
